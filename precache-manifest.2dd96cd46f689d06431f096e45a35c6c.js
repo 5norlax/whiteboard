48e061583cd287df3297ee805b5a7216268ab80c
nicklaslanman <nicklasllanman@gmail.com>
2020-08-27 23:25:47 -0500

    Updates

---
+++ precache-manifest.2dd96cd46f689d06431f096e45a35c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de7a5597cb131f8eb90ef0f1ac4e7da",
+    "url": "/whiteboard/index.html"
+  },
+  {
+    "revision": "4579145dad9da4a0af17",
+    "url": "/whiteboard/static/css/main.5dda5a91.chunk.css"
+  },
+  {
+    "revision": "eb945619bc4181fa1ae8",
+    "url": "/whiteboard/static/js/2.bdee68e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whiteboard/static/js/2.bdee68e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4579145dad9da4a0af17",
+    "url": "/whiteboard/static/js/main.a37d5436.chunk.js"
+  },
+  {
+    "revision": "95f52f808f7b1ca668e1",
+    "url": "/whiteboard/static/js/runtime-main.c2696591.js"
+  }
+]);
